ded45677fa5db415d4e9e96b9c1c05894de7797b
huska0707 <hussaink001haled@gmail.com>
2024-07-09 13:14:16 -0500

    Navbar

---
+++ components/Navbar.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Image from "next/image";
+import Vector from "../public/assets/icons/Icons.svg";
+
+const Navbar = () => {
+  return (
+    <div className="flex w-full justify-center top-[50px]">
+      <nav className="bg-black text-white p-4 flex justify-between w-[1297px] h-[59px] rounded-[90px]">
+        <div className="text-3xl font-bold leading-[37.62px] font-Orbitron">
+          GIFTER
+        </div>
+        <div className="flex space-x-4 w-[781px] justify-between">
+          <div className="w-[412px] h-[40px] flex justify-between font-bold text-[15px] font-Manrope">
+            <a href="#future">Future</a>
+            <a href="#features">Features</a>
+            <a href="#marketplace">Marketplace</a>
+            <a href="#contact">Contact</a>{" "}
+          </div>
+          <button className="flex justify-center text-base font-medium leading-5 font-Inter w-[164px]">
+            Get Started
+            <Image priority src={Vector} alt="Icons" width={20} height={20} />
+          </button>{" "}
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
